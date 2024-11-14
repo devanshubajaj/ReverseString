ab39b44e1c61d439a32f1d99fc0fb9ecd456a3bd
Devanshu Bajaj <61451237+devanshubajaj@users.noreply.github.com>
2024-11-13 19:01:14 -0800

    Implemented feature vowels check

--- reverseString.java
+++ reverseString.java
@@ -10,11 +10,31 @@ class StringReverser {
         String reversed = reverseString(original);
         System.out.println("Reversed String: " + reversed);
 
+        // Count the number of vowels in the reversed string
+        int vowelCount = countVowels(reversed);
+        System.out.println("Number of vowels in the reversed string: " + vowelCount);
+
         scanner.close();
     }
 
     public static String reverseString(String str) {
         StringBuilder sb = new StringBuilder(str);
         return sb.reverse().toString();
+    }
+
+    // Method to count vowels in a string
+    public static int countVowels(String str) {
+        int count = 0;
+        for (char c : str.toLowerCase().toCharArray()) { // Convert to lowercase for case-insensitive counting
+            if (isVowel(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Helper method to check if a character is a vowel
+    public static boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
     }
 }
